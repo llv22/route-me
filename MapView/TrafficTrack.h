fcfccf14e742bdff704298459ecdcd3ee95eb532
Orlando Ding <xiandao.airs@gmail.com>
2012-08-30 21:06:16 +0800

    llv22 - restkit for new implementation

--- MapView/TrafficTrack.h
+++ MapView/TrafficTrack.h
@@ -5,6 +5,7 @@
 //  Created by Ding Orlando on 8/23/12.
 //
 //  refer to - https://groups.google.com/forum/?fromgroups=#!topic/route-me-map/MlNTVxfUyrw
+//  objective-c programming guide - http://www.oogtech.org/content/2009/10/22/objective-c-good-evil-things-to-know/
 //
 
 #import <Foundation/Foundation.h>
@@ -22,5 +23,7 @@ static int _wcategory = 0;
 @property (nonatomic, readwrite, getter = getWcategory, setter = setWcategory:) int wcategory;
 ///\des - get from speed?
 @property (nonatomic, readonly, getter = getUicolor) UIColor* uicolor;
+
+///\des - auto-release for http://stackoverflow.com/questions/6673477/objective-c-static-field-issue
 
 @end
